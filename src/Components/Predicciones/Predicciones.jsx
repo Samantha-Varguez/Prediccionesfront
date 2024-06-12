246dc5c6228982ee6d79489f3a90a101bc5da70e
samantha-varguez <varaguezsamy@gmail.com>
2024-06-11 19:16:41 -0600

    4to

--- src/Components/Predicciones/Predicciones.jsx
+++ src/Components/Predicciones/Predicciones.jsx
@@ -1,50 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import './Predicciones.css';
 
-const TableComponent = () => {
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      fetch('https://api.example.com/data') // Replace with your API URL
-        .then(response => response.json())
-        .then(data => {
-          setData(data);
-          setLoading(false);
-        })
-        .catch(error => {
-          console.error('Error fetching data:', error);
-          setLoading(false);
-        });
-    }, []);
-  
-    if (loading) {
-      return <div>Loading...</div>;
-    }
-  
-    return (
-      <table className="tabla">
-        <thead>
-          <tr>
-            {/* Adjust these headers based on your API response */}
-            <th> Fecha </th>
-            <th> Sintoma </th>
-            <th> Prediccion de IA </th>
+function MapView() {
+  // Estado para almacenar predicciones
+  const [predicciones, setPredicciones] = useState([]);
+
+  // useEffect para obtener los datos de las clínicas cuando el componente se monta
+  useEffect(() => {
+    const fetchPredicciones = async () => {
+      try {
+        const response = await fetch('/api/predicciones'); // Realiza una solicitud al backend
+        const data = await response.json(); // Convierte la respuesta a JSON
+        setPredicciones(data); // Actualiza el estado con los datos de las clínicas
+      } catch (error) {
+        console.error('Error fetching predicciones:', error); 
+      }
+    };
+
+    fetchPredicciones(); // Llama a la función para obtener los datos
+  }, []); // Se ejecuta solo una vez, al montar el componente
+
+  return (
+    <table className="tabla">
+      <thead>
+        <tr>
+          {/* Adjust these headers based on your API response */}
+          <th> Fecha </th>
+          <th> Sintoma </th>
+          <th> Prediccion de IA </th>
+        </tr>
+      </thead>
+      <tbody>
+        {predicciones.map(item => (
+          <tr key={item.id}> 
+            <td>{item.id}</td> 
+            <td>{item.name}</td>
+            <td>{item.value}</td>
           </tr>
-        </thead>
-        <tbody>
-          {data.map(item => (
-            <tr key={item.id}> 
-              <td>{item.id}</td> 
-              <td>{item.name}</td>
-              <td>{item.value}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  };
+        ))}
+      </tbody>
+    </table>
+  );
+
+
+}
+    
+
   
-  export default TableComponent;
+  export default MapView;
   
   
